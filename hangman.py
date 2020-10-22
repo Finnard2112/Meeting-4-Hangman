a7d0ced67c7ebe4241e97d6304328b55ce844bb8
Finnard2112 <73263802+Finnard2112@users.noreply.github.com>
2020-10-21 21:15:20 -0400

    meeting4_hangman.py
    It is my hangman code

---
+++ hangman.py
@@ -0,0 +1,26 @@
+answer = input("what's the word")
+answer_list = list(answer) #list version of the original word
+presentation = []
+for i in range(len(answer_list)):
+    presentation.append("_") #makes a list that shows the progress of the player during the game
+
+completion = False # condition for end game
+while completion == False:
+    attempt = input('guess')
+    ind = 0 #index of the guess that appears in answer_list
+    count = 0 #number of occurences of the guess in answer_list
+    for x in answer_list: #searches for all occurences of the guess in answer_list and change presentation accordingly
+        if x == attempt:
+            num = answer_list.index(attempt)
+            presentation[num] = attempt
+            answer_list[num] = 0 #if there is an occurence, replace that occurence with 0 in answer_list
+            count += 1
+    
+    if count>0:
+        print ("Your guess is correct, there was/were {} matches in the word".format(count))
+        print(presentation)
+    
+    if any(answer_list) == False: #since all 0 have a negative truthy value, we can use all() to check if any element has a truthy value 
+        completion = True
+
+print("Congrats, you got everything correct")
